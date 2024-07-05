a28742deb6a6f89112b3170e82180fd9e003fdf0
Yajirang <ubiboy85@gmail.com>
2024-07-05 16:25:42 +0900

    modify metadata

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,10 @@ const frameMetadata = getFrameMetadata({
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
+  title: 'Wild Allowance check',
   description: 'LFG',
   openGraph: {
-    title: 'zizzamia.xyz',
+    title: 'Wild Allowance check',
     description: 'LFG',
     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
