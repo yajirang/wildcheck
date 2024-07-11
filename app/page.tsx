9ecaf23b676c5fb5eb120bd2a5e65d04f01ab8ab
Yajirang <ubiboy85@gmail.com>
2024-07-11 19:17:01 +0900

    Change buttons

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,19 @@ import { NEXT_PUBLIC_URL } from './config';
 
 const frameMetadata = getFrameMetadata({
   buttons: [
-    {
-      label: 'Story time',
-    },
-
+    
     {
       //action: 'tx',
       label: 'Check Allowance',
       //target: `${NEXT_PUBLIC_URL}/api/tx`,
       //postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
     }, 
+
+    {
+      label: 'My tickets',
+    },
+
+    
 
     /* {
       action: 'tx',
@@ -29,7 +32,7 @@ const frameMetadata = getFrameMetadata({
 /*   input: {
     text: 'Tell me a story',
   }, */
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame_allow`,
 });
 
 export const metadata: Metadata = {
